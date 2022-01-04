943d1eb7fef6adae04d4da0ca713d7122fde793b
Mattia Polli <77399568+MattiaPolli93@users.noreply.github.com>
2022-01-04 15:30:57 +0100

    Implement and render search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,12 +1,16 @@
-// API at "https://forkify-api.herokuapp.com/v2"
-import * as model from "./model.js"
-import recipeView from "./views/recipeView.js"
+import * as model from "./model.js";
+import recipeView from "./views/recipeView.js";
+import searchView from "./views/searchView.js";
+import resultsView from "./views/resultsView.js";
 
 // Polyfill
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-const recipeContainer = document.querySelector(".recipe");
+// Parcel
+if (module.hot) {
+    module.hot.accept();
+}
 
 const controlRecipes = async function() {
     try {
@@ -27,7 +31,26 @@ const controlRecipes = async function() {
     }
 };
 
+const controlSearchResults = async function() {
+    try {
+        resultsView.renderSpinner();
+
+        // Get search query
+        const query = searchView.getQuery();
+        if (!query) return;
+
+        // Load search results
+        await model.loadSearchResults(query);
+
+        // Render results
+        resultsView.render(model.state.search.results);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
 const init = function() {
     recipeView.addHandlerRender(controlRecipes);
+    searchView.addHandlerSearch(controlSearchResults)
 };
 init();
