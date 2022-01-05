aa852334244279be75f1fd23c6851f632f13a480
Mattia Polli <77399568+MattiaPolli93@users.noreply.github.com>
2022-01-05 14:54:33 +0100

    Implement pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,15 +2,16 @@ import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
 import searchView from "./views/searchView.js";
 import resultsView from "./views/resultsView.js";
+import paginationView from "./views/paginationView.js";
 
 // Polyfill
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
 // Parcel
-if (module.hot) {
+/* if (module.hot) {
     module.hot.accept();
-}
+} */
 
 const controlRecipes = async function() {
     try {
@@ -35,22 +36,34 @@ const controlSearchResults = async function() {
     try {
         resultsView.renderSpinner();
 
-        // Get search query
+        // 1) Get search query
         const query = searchView.getQuery();
         if (!query) return;
 
-        // Load search results
+        // 2) Load search results
         await model.loadSearchResults(query);
 
-        // Render results
-        resultsView.render(model.state.search.results);
+        // 3) Render results
+        resultsView.render(model.getSearchResultsPage());
+
+        // 4) Render initial pagination buttons
+        paginationView.render(model.state.search);
     } catch (err) {
         console.log(err);
     }
 };
 
+const controlPagination = function(goToPage) {
+    // 1) Render new results
+    resultsView.render(model.getSearchResultsPage(goToPage));
+
+    // 2) Render new pagination buttons
+    paginationView.render(model.state.search);
+};
+
 const init = function() {
     recipeView.addHandlerRender(controlRecipes);
-    searchView.addHandlerSearch(controlSearchResults)
+    searchView.addHandlerSearch(controlSearchResults);
+    paginationView.addHandlerClick(controlPagination);
 };
 init();
